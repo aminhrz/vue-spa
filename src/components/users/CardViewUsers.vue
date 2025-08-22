7fdbfe970c647f52c207ad4a1a91735608cf03bc
ehsanhrz <amin76heidarzadeh@gmail.com>
2025-08-22 21:13:20 +0330

    fix user link in usershow

--- src/components/users/CardViewUsers.vue
+++ src/components/users/CardViewUsers.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="card mb-4">
     <div class="card-header">
-      <router-link :to="{ name: 'idUsers', params: { id: user.id } }">{{ user.name }}</router-link>
+      <template v-if="$route.name === 'idUsers' && $route.params.id == user.id">
+        <h5>{{ user.name }}</h5>
+      </template>
+      <template v-else>
+        <router-link :to="{ name: 'idUsers', params: { id: user.id } }">
+          {{ user.name }}
+        </router-link>
+      </template>
     </div>
+
     <ul class="list-group list-group-flush">
       <li class="list-group-item">{{ user.username }}</li>
       <li class="list-group-item">{{ user.email }}</li>
